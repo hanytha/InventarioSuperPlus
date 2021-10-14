4efa85e8c2a3862ca5b435b9e9493bb18cd6f007
AnabelAquinoCaballero <leb2198@gmail.com>
2021-10-14 17:04:18 -0500

    Se agregaron validaciones en compra, proveedor y existencias

---
+++ Inventario/Scrips/General/JS_Validacion.js
@@ -0,0 +1,179 @@
+﻿//---------Validación existencias almacen---------
+function validarFormulario() {
+
+    var TxtNumCompra = document.getElementById('TxtNumCompra').value;
+    var TxtExistenciaInicial = document.getElementById('TxtExistenciaInicial').value;
+    var TxtExistenciaActual = document.getElementById('TxtExistenciaActual').value;
+    var TxtCosto = document.getElementById('TxtCosto').value;
+
+
+    //Test edad
+    if (TxtNumCompra == null || TxtNumCompra.length == 0 || isNaN(TxtNumCompra)) {
+        alert('ERROR: Debe ingresar un Número de compra');
+        return false;
+    }
+    //Test edad
+    if (TxtExistenciaInicial == null || TxtExistenciaInicial.length == 0 || isNaN(TxtExistenciaInicial)) {
+        alert('ERROR: Debe ingresar una Existencia Inicial');
+        return false;
+    }
+    //Test edad
+    if (TxtExistenciaActual == null || TxtExistenciaActual.length == 0 || isNaN(TxtExistenciaActual)) {
+        alert('ERROR: Debe ingresar una Existencia Actual');
+        return false;
+    }
+    //Test edad
+    if (TxtCosto == null || TxtCosto.length == 0 || isNaN(TxtCosto)) {
+        alert('ERROR: Debe ingresar un Costo');
+        return false;
+    }
+
+
+    return true;
+}
+
+
+//----------------Validación Formulario Compra-------------------------
+function validarFormularioCompra() {
+
+    var TxtMetodoDePago = document.getElementById('TxtMetodoDePago').value;
+
+    //Test campo obligatorio
+    if (TxtMetodoDePago == null || TxtMetodoDePago.length == 0 || /^\s+$/.test(TxtMetodoDePago)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+        return false;
+    }
+
+    return true;
+}
+//------------------Validación proveedor-------------
+
+function validarFormularioProveedor() {
+
+    var TxtNombre = document.getElementById('TxtNombre').value;
+    var Txtcorreo = document.getElementById('Txtcorreo').value;
+    var TxtRazonSocial = document.getElementById('TxtRazonSocial').value;
+    var TxtClaveInterbancaria = document.getElementById('TxtClaveInterbancaria').value;
+    var TxtCodigoPostal = document.getElementById('TxtCodigoPostal').value;
+    var cmbEstado = document.getElementById('cmbEstado').selectedIndex;
+    var cmbMunicipio = document.getElementById('cmbMunicipio').selectedIndex;
+    var cmbLocalidad = document.getElementById('cmbLocalidad').selectedIndex;
+    var TxtRFC = document.getElementById('TxtRFC').value;
+    var TxtDireccion = document.getElementById('TxtDireccion').value;
+    var TxtTelefono = document.getElementById('TxtTelefono').value;
+    var TxtBanco = document.getElementById('TxtBanco').value;
+    var TxtNumeroDeCuenta = document.getElementById('TxtNumeroDeCuenta').value;
+    var TxtUsoCFDI = document.getElementById('TxtUsoCFDI').value;
+    var TxtNomenclatura = document.getElementById('TxtNomenclatura').value;
+    var TxtDescripcion = document.getElementById('TxtDescripcion').value;
+
+
+
+    //Test campo obligatorio
+    if (TxtNombre == null || TxtNombre.length == 0 || /^\s+$/.test(TxtNombre )) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+        return false;
+    }
+    //Test correo
+    if (!(/\S+@\S+\.\S+/.test(Txtcorreo))) {
+        alert('ERROR: Debe escribir un correo válido');
+        return false;
+    }
+
+    //Test campo obligatorio
+    if (TxtRazonSocial == null || TxtRazonSocial.length == 0 || /^\s+$/.test(TxtRazonSocial)) {
+        alert('ERROR: El campo nombre no debe ir vacío TxtRazonSocial');
+        return false;
+    }
+
+    //Test edad
+    if (TxtClaveInterbancaria == null || TxtClaveInterbancaria.length == 0 || isNaN(TxtClaveInterbancaria)) {
+        alert('ERROR: Debe ingresar una edad TxtClaveInterbancaria');
+        return false;
+    }
+
+    //Test edad
+    if (TxtCodigoPostal == null || TxtCodigoPostal.length == 0 || isNaN(TxtCodigoPostal)) {
+        alert('ERROR: Debe ingresar una edad TxtCodigoPostal');
+        return false;
+    }
+    //Test comboBox
+    if (cmbEstado == null || cmbEstado == 0) {
+        alert('ERROR: Debe seleccionar una opcion del combo box cmbEstado');
+        return false;
+    }
+    //Test comboBox
+    if (cmbMunicipio == null || cmbMunicipio == 0) {
+        alert('ERROR: Debe seleccionar una opcion del combo box cmbMunicipio');
+        return false;
+    }
+    //Test comboBox
+    if (cmbLocalidad == null || cmbLocalidad == 0) {
+        alert('ERROR: Debe seleccionar una opcion del combo box cmbLocalidad');
+        return false;
+    }
+    //Test campo obligatorio
+    if (TxtRFC == null || TxtRFC.length == 0 || /^\s+$/.test(TxtRFC)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco TxtRFC');
+        return false;
+    }
+    //Test campo obligatorio
+    if (TxtDireccion == null || TxtDireccion.length == 0 || /^\s+$/.test(TxtDireccion)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco TxtRFC TxtDireccion');
+        return false;
+    }
+    //Test edad
+    if (TxtTelefono == null || TxtTelefono.length == 0 || isNaN(TxtTelefono)) {
+        alert('ERROR: Debe ingresar una edad TxtTelefono');
+        return false;
+    }
+
+    //Test campo obligatorio
+    if (TxtBanco == null || TxtBanco.length == 0 || /^\s+$/.test(TxtBanco)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco TxtBanco');
+        return false;
+    }
+
+
+    //Test edad
+    if (TxtNumeroDeCuenta == null || TxtNumeroDeCuenta.length == 0 || isNaN(TxtNumeroDeCuenta)) {
+        alert('ERROR: Debe ingresar una edad TxtNumeroDeCuenta');
+        return false;
+    }
+
+    //Test edad
+    if (TxtUsoCFDI == null || TxtUsoCFDI.length == 0 || isNaN(TxtUsoCFDI)) {
+        alert('ERROR: Debe ingresar una TxtUsoCFDI');
+        return false;
+    }
+
+
+    //Test campo obligatorio
+    if (TxtNomenclatura == null || TxtNomenclatura.length == 0 || /^\s+$/.test(TxtNomenclatura)) {
+        alert('ERROR: El campo nombre no debe ir vacío TxtNomenclatura');
+        return false;
+    }
+    //Test campo obligatorio
+    if (TxtDescripcion == null || TxtDescripcion.length == 0 || /^\s+$/.test(TxtDescripcion)) {
+        alert('ERROR: El campo nombre no debe ir vacío TxtDescripcion');
+        return false;
+    }
+    
+
+    return true;
+}
+//----------------Validación Formulario categorías-------------------------
+function validarFormularioCategorias() {
+
+    var TxtClasificacion = document.getElementById('TxtClasificacion').value;
+
+    //Test campo obligatorio
+    if (TxtClasificacion == null || TxtClasificacion.length == 0 || /^\s+$/.test(TxtClasificacion)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco TxtClasificacion');
+        return false;
+    }
+
+    return true;
+}   
+
+//---------------------------------------------------------------
--- Inventario/Scrips/Internos/JS_Departamentos.js
+++ Inventario/Scrips/Internos/JS_Departamentos.js
@@ -1,85 +1,53 @@
-﻿
-CrearAcordeonDepartamentos();
+﻿CrearAcordeonDepartamentos();
 //Crea el acordeón e inserta (los registros de la base de datos)
 function CrearAcordeonDepartamentos() {
-    $.get("/Departamentos/ConsultaDepartamentos", function (DatosDepartamentos) {
-        //Accordeon(DatosDepartamentos, document.getElementById("accordion"));
-        AcordeonDepartamentos(DatosDepartamentos, document.getElementById("accordion"));
+    $.get("/Departamentos/ConsultaDepartamentos", function (Data) {
+        //Accordeon(DatosProveedor, document.getElementById("accordion"));
+        AcordeonDepartamentos(Data, document.getElementById("accordion"));
     });
 }
-function AcordeonDepartamentos(DatosDepartamentos, CtrlDepartamentos) {
-    var CodigoHTMLDepartamentos = "";
-    for (var i = 0; i < DatosDepartamentos.length; i++) {
+function AcordeonDepartamentos(Data, CtrlAlmacen) {
+    var CodigoHTMLAreas = "";
+    for (var i = 0; i < Data.length; i++) {
         if (i < 1) {
-            CodigoHTMLDepartamentos += "<div class='card m-b-0'>";
+            CodigoHTMLAreas += "<div class='card m-b-0'>";
         }
         else {
-            CodigoHTMLDepartamentos += "<div class='card m-b-0 border-top'>";
+            CodigoHTMLAreas += "<div class='card m-b-0 border-top'>";
         }
-        CodigoHTMLDepartamentos += "<div class='card-header' id='heading" + DatosDepartamentos[i].Id + "'>";
-        CodigoHTMLDepartamentos += "<h5 class='mb-0'>";
-        CodigoHTMLDepartamentos += "<a  data-toggle='collapse' data-target='#collapse" + DatosDepartamentos[i].Id + "' aria-expanded='false' aria-controls='collapse" + DatosDepartamentos[i].Id + "' class='collapsed'>";
-        //CodigoHTMLDepartamentos += "<i class='m-r-5 mdi mdi-store' aria-hidden='true'></i>";
-        CodigoHTMLDepartamentos += "<i class='m-r-5 fas fa-clipboard-list' aria-hidden='true'></i>";
-        CodigoHTMLDepartamentos += "<span >" + DatosDepartamentos[i].Nombre + "</span>";
-        CodigoHTMLDepartamentos += "</a>";
-        CodigoHTMLDepartamentos += "</h5>";
+        CodigoHTMLAreas += "<div class='card-header' id='heading" + Data[i].Id + "'>";
+        CodigoHTMLAreas += "<h5 class='mb-0'>";
+        CodigoHTMLAreas += "<a  data-toggle='collapse' data-target='#collapse" + Data[i].Id + "' aria-expanded='false' aria-controls='collapse" + Data[i].Id + "' class='collapsed'>";
+        //CodigoHTMLAreas += "<i class='m-r-5 mdi mdi-store' aria-hidden='true'></i>";
+        CodigoHTMLAreas += "<i class='m-r-5 fas fa-clipboard-list' aria-hidden='true'></i>";
+        CodigoHTMLAreas += "<span >" + Data[i].Nombre + "</span>";
+        CodigoHTMLAreas += "</a>";
+        CodigoHTMLAreas += "</h5>";
         //En el data-parent se modifica para que se de un solo clic y se oculten los demás
-        CodigoHTMLDepartamentos += "<div id='collapse" + DatosDepartamentos[i].Id + "' class='collapse' aria-labelledby='headingOne' data-parent='#collapse' style=''>";
-        CodigoHTMLDepartamentos += "<div class='card-body'>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>UNombre: </strong>" + DatosDepartamentos[i].UNombre + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Correo: </strong>" + DatosDepartamentos[i].Correo + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Telefono: </strong>" + DatosDepartamentos[i].Telefono + "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Cuenta Interbancaria: </strong>" + DatosDepartamentos[i].   
-            
-            + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Código Postal: </strong>" + DatosDepartamentos[i].CodigoPostal + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Estado: </strong>" + DatosDepartamentos[i].Estado + "</div>";
-        //  CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Dirección: </strong>" + DatosDepartamentos[i].Direccion + "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        //CodigoHTMLDepartamentos += "<div class='col-md-12 col-sm-12 col-xs-12'><strong>Dirección</strong></div >";
-        //CodigoHTMLDepartamentos += "</div>";
-        //CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>RFC: </strong>" + DatosDepartamentos[i].RFC + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Dirección: </strong>" + DatosDepartamentos[i].Direccion + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Localidad: </strong>" + DatosDepartamentos[i].Localidad + "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-12 col-sm-6 col-xs-6'><strong>Teléfono: </strong>" + DatosDepartamentos[i].Telefono + "</div>";
+        CodigoHTMLAreas += "<div id='collapse" + Data[i].Id + "' class='collapse' aria-labelledby='headingOne' data-parent='#collapse' style=''>";
+        CodigoHTMLAreas += "<div class='card-body'>";
+        CodigoHTMLAreas += "<div class='row'>";
+        CodigoHTMLAreas += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Correo: </strong>" + Data[i].UNombre + "</div>";
+        CodigoHTMLAreas += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Giro del Proveedor: </strong>" + Data[i].Correo + "</div>";
+        CodigoHTMLAreas += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Municipio: </strong>" + Data[i].Telefono + "</div>";
+        CodigoHTMLAreas += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Municipio: </strong>" + Data[i].Carpeta + "</div>";
+        CodigoHTMLAreas += "</div>";
 
-        // CodigoHTMLDepartamentos += "</div>";
-        // CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-12 col-sm-6 col-xs-6'><strong>Banco: </strong>" + DatosDepartamentos[i].Banco + "</div>";
+        //  CodigoHTMLAreas += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Dirección: </strong>" + DatosProveedor[i].Direccion + "</div>";
 
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Teléfono: </strong>" + DatosDepartamentos[i].Telefono + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Número de Cuenta: </strong>" + DatosDepartamentos[i].NumeroDeCuenta + "</div>";
 
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>UsoCFDI: </strong>" + DatosDepartamentos[i].UsoCFDI + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Nomenclatura: </strong>" + DatosDepartamentos[i].Nomenclatura + "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='row'>";
-        CodigoHTMLDepartamentos += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Descripción: </strong>" + DatosDepartamentos[i].Descripcion + "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Logo: </strong>" + DatosDepartamentos[i].Logo + "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        //CodigoHTMLDepartamentos += "<button class='btn btn-info' onclick='MostrarOcultar(" + DatosDepartamentos[i].ID + ")'><i id='BtnMO" + DatosDepartamentos[i].Id + "' class='fas fa-chevron-circle-down'></i></button></div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "<div class='col-md-12 col-sm-12 col-xs-12 align-self-end'>";
-        CodigoHTMLDepartamentos += "<button class='btn btn-success' onclick='AbrirMProveedores(" + DatosDepartamentos[i].Id + ")' data-toggle='modal' data-target='#Proveedores'><i class='fas fa-edit'></i></button> ";
-        CodigoHTMLDepartamentos += "<button class='btn btn-danger' onclick='EliminarProveedores(" + DatosDepartamentos[i].Id + ",this)' ><i class='fas fa-eraser'></i></button>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "</div>";
-        CodigoHTMLDepartamentos += "</div>";
+        //CodigoHTMLAreas += "<button class='btn btn-info' onclick='MostrarOcultar(" + DatosProveedor[i].ID + ")'><i id='BtnMO" + DatosProveedor[i].Id + "' class='fas fa-chevron-circle-down'></i></button></div>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "<div class='col-md-12 col-sm-12 col-xs-12 align-self-end'>";
+        CodigoHTMLAreas += "<button class='btn btn-success' onclick='abrirModal(" + Data[i].Id + ")' data-toggle='modal' data-target='#dialogo1'><i class='fas fa-edit'></i></button> ";
+        CodigoHTMLAreas += "<button class='btn btn-danger' onclick='EliminarExistenciasG(" + Data[i].Id + ",this)' ><i class='fas fa-eraser'></i></button>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "</div>";
+        CodigoHTMLAreas += "</div>";
     }
-    CtrlDepartamentos.innerHTML = CodigoHTMLDepartamentos;
+    CtrlAlmacen.innerHTML = CodigoHTMLAreas;
 }
+
