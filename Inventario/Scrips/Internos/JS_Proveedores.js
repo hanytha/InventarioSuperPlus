9f7a15feb663bfa9078e18cacec9d85ec306d277
AlmaLeticiaHernandezMartinez <alma.ytel@gmail.com>
2021-10-16 10:48:23 -0500

    Función para regresar el formulario del mnodal al inicio al presionar el botón cancelar

--- Inventario/Scrips/Internos/JS_Proveedores.js
+++ Inventario/Scrips/Internos/JS_Proveedores.js
@@ -307,24 +307,48 @@ function EliminarProveedores(id) {
     }
 }
 
-/*
+////Función para regresar el formulario del mnodal al inicio al presionar el botón cancelar////
 (function () {
     var template = null
     $('.modal').on('show.bs.modal', function (event) {
         if (template == null) {//Valores nulos
             template = $(this).html()
+
         } else {
             $(this).html(template)
+
+            $(document).ready(function () {
+                var current = 1, current_step, next_step, steps;
+                steps = $("fieldset").length;
+                $(".next").click(function () {
+                    current_step = $(this).parent();
+                    next_step = $(this).parent().next();
+                    next_step.show();
+                    current_step.hide();
+                    setProgressBar(++current);
+                });
+                $(".previous").click(function () {
+                    current_step = $(this).parent();
+                    next_step = $(this).parent().prev();
+                    next_step.show();
+                    current_step.hide();
+                    setProgressBar(--current);
+                });
+                setProgressBar(current);
+                // Change progress bar action
+                function setProgressBar(curStep) {
+                    var percent = parseFloat(100 / steps) * curStep;
+                    percent = percent.toFixed();
+                    $(".progress-bar")
+                        .css("width", percent + "%")
+                        .html(percent + "%");
+                }
+            });
         }
         // other initialization here, if you want to
     })
 
 })()
-*/
-
-
-
-
 
 
 
