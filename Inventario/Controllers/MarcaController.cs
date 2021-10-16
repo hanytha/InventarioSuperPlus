6c822fe580d4d4b42fc3f58f901bde53982067af
AnabelAquinoCaballero <leb2198@gmail.com>
2021-10-16 17:31:06 -0500

    Se modifico el menú y se agrego el carrusel

--- Inventario/Controllers/MarcaController.cs
+++ Inventario/Controllers/MarcaController.cs
@@ -8,10 +8,98 @@ namespace Inventario.Controllers
 {
     public class MarcaController : Controller
     {
+        //conexion con DB
+        InventarioBDDataContext InvBD = new InventarioBDDataContext();
         // GET: Marca
         public ActionResult Marca()
         {
             return View();
         }
+        public JsonResult ConsultaMarcas()
+        {
+            var Marcas = InvBD.Marca.Where(p => p.Estatus.Equals(1))
+                .Select(p => new
+                {
+                    p.Id,
+                    p.Nombre,
+                });
+            return Json(Marcas, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ConsultaMarca(long Id)
+        {
+            var Marca = InvBD.Marca.Where(p => p.Id.Equals(Id))
+                .Select(p => new
+                {
+                    p.Id,
+                    p.Nombre,
+                });
+            return Json(Marca, JsonRequestBehavior.AllowGet);
+        }
+        //Guardar los datos de la compra
+        public int GuardarMarca(Marca DatosMarca)
+        {
+            int Afectados = 0;
+            //try
+            //{
+            long id = DatosMarca.Id;
+            if (id.Equals(0))
+            {
+                int nveces = InvBD.Marca.Where(p => p.Nombre.Equals(DatosMarca.Nombre)).Count();
+
+                // int nveces = InvBD.Proveedores.Where(p => p.Nombre.Equals(DatosProveedor.Nombre) && p.Correo.Equals(DatosProveedor.Correo) && p.RazonSocial.Equals(DatosProveedor.RazonSocial) && p.ClaveInterbancaria.Equals(DatosProveedor.ClaveInterbancaria) && p.CodigoPostal.Equals(DatosProveedor.CodigoPostal) && p.RFC.Equals(DatosProveedor.RFC) && p.Direccion.Equals(DatosProveedor.Direccion) && p.Telefono.Equals(DatosProveedor.Telefono) && p.Banco.Equals(DatosProveedor.Banco) && p.NumeroDeCuenta.Equals(DatosProveedor.NumeroDeCuenta) && p.UsoCFDI.Equals(DatosProveedor.UsoCFDI) && p.Nomenclatura.Equals(DatosProveedor.Nomenclatura)).Count();
+                if (nveces == 0)
+                {
+                    InvBD.Marca.InsertOnSubmit(DatosMarca);
+                    InvBD.SubmitChanges();
+                    Afectados = 1;
+                }
+                else
+                {
+                    Afectados = -1;
+                }
+            }
+            else
+            {
+                int nveces = InvBD.Marca.Where(p => p.Nombre.Equals(DatosMarca.Nombre)).Count();
+                if (nveces == 0)
+                {
+                    Compra obj = InvBD.Compra.Where(p => p.Id.Equals(id)).First();
+                    obj.MetodoDePago = DatosMarca.Nombre;
+                    InvBD.SubmitChanges();
+                    Afectados = 1;
+                }
+                else
+                {
+                    Afectados = -1;
+                }
+            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Afectados = 0;
+            //}
+            return Afectados;
+        }
+
+
+
+        //Eliminar Compra
+        public int EliminarMarca(long Id)
+        {
+            int nregistradosAfectados = 0;
+            try
+            {//Consulta los datos y el primer Id que encuentra  lo compara
+                Marca mar = InvBD.Marca.Where(p => p.Id.Equals(Id)).First();
+                mar.Estatus = 0;//Cambia el estatus en 0
+                InvBD.SubmitChanges();//Guarda los datos en la Base de datos
+                nregistradosAfectados = 1;//Se pudo realizar
+            }
+            catch (Exception ex)
+            {
+                nregistradosAfectados = 0;
+            }
+            return nregistradosAfectados;
+        }
+
     }
 }
---
+++ Inventario/Scrips/Internos/JS_Marca.js
@@ -0,0 +1,134 @@
+﻿ConsultaMarcas();
+function ConsultaMarcas() {
+    $.get("/Marca/ConsultaMarcas", function (Data) {
+        CrearTablaMarcas(Data);
+    }
+    );
+}
+function CrearTablaMarcas(Data) {
+    var CodigoHtmlTablaMarcas = "";
+    CodigoHtmlTablaMarcas  += "<table id='tablas' class='table table table-sm'>";
+    CodigoHtmlTablaMarcas  += "<thead><tr><th>Nombre</th><th>Acción</thead>";
+    CodigoHtmlTablaMarcas  += "<tbody>";
+    for (var i = 0; i < Data.length; i++) {
+        CodigoHtmlTablaMarcas  += "<tr>";
+        CodigoHtmlTablaMarcas  += "<td>" + Data[i].Nombre + "</td>";
+
+        CodigoHtmlTablaMarcas  += "<td>";
+        CodigoHtmlTablaMarcas += "<button class='btn btn-primary' onclick='editarModal(" + Data[i].Id + ")' data-toggle='modal' data-target='#dialogo1'><i class='fas fa-edit'></i></button>";
+        CodigoHtmlTablaMarcas += "<button class='btn btn-danger' onclick='EliminarMarca(" + Data[i].Id + ",this)'><i class='fas fa-eraser'></i></button>";
+
+        CodigoHtmlTablaMarcas  += "</td>";
+        CodigoHtmlTablaMarcas  += "</tr>";
+    }
+    CodigoHtmlTablaMarcas += "</tbody>";
+    CodigoHtmlTablaMarcas  += "</table>";
+    document.getElementById("tabla").innerHTML = CodigoHtmlTablaMarcas ;
+}
+
+
+//Limpia la información y carga la informacion de la compra
+function editarModal(id) {//la clase AreaObligatorio
+    var controlesObligatorio = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligatorio.length;
+    for (var i = 0; i < ncontroles; i++) {//recorre
+        controlesObligatorio[i].parentNode.classList.remove("border-danger");//Cambia los bordes lo las casillas a color rojo
+    }
+    if (id == 0) {
+
+        LimpiarCampos();
+        sessionStorage.setItem('IDMarca', '0');
+    }
+    else {
+
+        $.get("/Marca/ConsultaMarca/?Id=" + id, function (Data) {
+            sessionStorage.setItem('IDMarca', Data[0].Id);
+
+            document.getElementById("TxtNombre").value = Data[0].Nombre;
+        });
+    }
+}
+
+//limpiar campos
+function LimpiarCampos() {
+    var controlesTXT = document.getElementsByClassName("limpiar");
+    for (var i = 0; i < controlesTXT.length; i++) {
+        controlesTXT[i].value = "";
+    }
+    var controlesSLT = document.getElementsByClassName("limpiarSelect");
+    for (var i = 0; i < controlesSLT.length; i++) {
+        controlesSLT[i].value = "0";
+    }
+}
+
+//Guarda los cambios y altas de las compras
+function GuardarMarca() {
+    if (CamposObligatorios() == true) {
+        if (confirm("¿Desea aplicar los cambios?") == 1) {
+            var Id = sessionStorage.getItem('IDMarca')
+            var Nombre = document.getElementById("TxtNombre").value;
+
+            var frm = new FormData();
+            frm.append("Id", Id);
+            frm.append("Nombre", Nombre);
+            frm.append("Estatus", 1);
+
+            $.ajax({
+                type: "POST",
+                url: "/Marca/GuardarMarca",
+                data: frm,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    if (data == 0) {
+                        alert("Ocurrio un error");
+                    }
+                    else if (data == -1) {
+                        alert("Ya existe Marca");
+                    }
+                    else {
+                        alert("Se ejecuto correctamente");
+                        ConsultaMarcas();
+                        document.getElementById("btnCancelar").click();
+                    }
+                }
+            });
+        }
+    }
+}
+
+//marca los campos obligatorios
+function CamposObligatorios() {
+    var exito = true;
+    var controlesObligatorio = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligatorio.length;
+    for (var i = 0; i < ncontroles; i++) {
+        if (controlesObligatorio[i].value == "") {
+            exito = false;
+            controlesObligatorio[i].parentNode.classList.add("error");
+        }
+        else {
+            controlesObligatorio[i].parentNode.classList.remove("error");
+        }
+    }
+    return exito;
+}
+
+//"Elimina" la compra cambia el Estatus
+function EliminarMarca(id) {
+    if (confirm("¿Desea eliminar el registro?") == 1) {
+
+        $.get("/Marca/EliminarMarca/?Id=" + id, function (DatoMarca) {
+            if (DatoMarca == 1) {
+                alert("Se elimino correctamente");
+                ConsultaMarcas();
+            } else {
+                alert("Ocurrio un error");
+            }
+        });
+    }
+}
+
+
+
+
