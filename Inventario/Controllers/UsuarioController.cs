7a82486697f0b60f4033c5001d077b5fcbd196f4
AlmaLeticiaHernandezMartinez <alma.ytel@gmail.com>
2021-11-03 11:12:01 -0600

    Se realizaron modificaciones en el modal Información General de Usuarios

--- Inventario/Controllers/UsuarioController.cs
+++ Inventario/Controllers/UsuarioController.cs
@@ -147,52 +147,29 @@ namespace Inventario.Controllers
             else
             {
                 int nveces = InvBD.Usuarios.Where(p => p.CURP.Equals(DatosUsuarios.CURP)
-                && p.Nombre.Equals(DatosUsuarios.Nombre) && p.ApellidosP.Equals(DatosUsuarios.ApellidosP)
                 && p.IdArea.Equals(DatosUsuarios.IdArea) && p.IdSubArea.Equals(DatosUsuarios.IdSubArea)
-                && p.ApellidosM.Equals(DatosUsuarios.ApellidosM) && p.FechaDeNacimiento.Equals(DatosUsuarios.FechaDeNacimiento)
                 && p.IdPerfil.Equals(DatosUsuarios.IdPerfil) && p.IdEstado.Equals(DatosUsuarios.IdEstado)
                 && p.IdMunicipio.Equals(DatosUsuarios.IdMunicipio) && p.IdLocalidad.Equals(DatosUsuarios.IdLocalidad)
-                && p.RFC.Equals(DatosUsuarios.RFC) && p.NoSS.Equals(DatosUsuarios.NoSS)
-                && p.Correo.Equals(DatosUsuarios.Correo) && p.Telefono.Equals(DatosUsuarios.Telefono)
-                && p.Telefono.Equals(DatosUsuarios.Telefono) && p.LvlPerfil.Equals(DatosUsuarios.LvlPerfil)
-                && p.Usuario.Equals(DatosUsuarios.Usuario) && p.IdArea.Equals(DatosUsuarios.IdArea)
-                && p.IdSubArea.Equals(DatosUsuarios.IdSubArea) && p.NArea.Equals(DatosUsuarios.NArea)
-                && p.NSArea.Equals(DatosUsuarios.NSArea)
                 && p.Foto.Equals(DatosUsuarios.Foto) && p.Telefono.Equals(DatosUsuarios.Telefono)).Count();
 
                 if (nveces == 0)
                 {
                     Usuarios obj = InvBD.Usuarios.Where(p => p.IdUsuarios.Equals(id)).First();
-                    obj.CURP = DatosUsuarios.CURP;
                     obj.Nombre = DatosUsuarios.Nombre;
                     obj.ApellidosP = DatosUsuarios.ApellidosP;
                     obj.ApellidosM = DatosUsuarios.ApellidosM;
-                    obj.FechaDeNacimiento = DatosUsuarios.FechaDeNacimiento;
-                    obj.IdEstado = DatosUsuarios.IdEstado;
-                    obj.IdMunicipio = DatosUsuarios.IdMunicipio;
-                    obj.IdLocalidad = DatosUsuarios.IdLocalidad;
-                    obj.RFC = DatosUsuarios.RFC;
-                    obj.NoSS = DatosUsuarios.NoSS;
-                    obj.Correo = DatosUsuarios.Correo;
-                    obj.Telefono = DatosUsuarios.Telefono;
-                    obj.IdPerfil = DatosUsuarios.IdPerfil;
-                    obj.LvlPerfil = DatosUsuarios.LvlPerfil;
-                    obj.Usuario = DatosUsuarios.Usuario;
-                    obj.FechaIngreso = DatosUsuarios.FechaIngreso;
-                    obj.Estado = DatosUsuarios.Estado;
-                    obj.Municipio = DatosUsuarios.Municipio;
-                    obj.Localidad = DatosUsuarios.Localidad;
                     obj.Password = DatosUsuarios.Password;
                     obj.IdArea = DatosUsuarios.IdArea;
                     obj.IdSubArea = DatosUsuarios.IdSubArea;
-                    obj.NArea = DatosUsuarios.NArea;
-                    obj.NSArea = DatosUsuarios.NSArea;
+                    obj.Foto = Convert.FromBase64String(cadF);
+                    obj.IdPerfil = DatosUsuarios.IdPerfil;
                     obj.IdMunicipio = DatosUsuarios.IdMunicipio;
                     //obj.NombreM = usuario.NombreM;
                     //obj.NombreL = usuario.NombreL;
-                    obj.IdEstado = DatosUsuarios.IdEstado;
                     obj.IdLocalidad = DatosUsuarios.IdLocalidad;
-                    obj.Foto = Convert.FromBase64String(cadF);
+                    obj.Correo = DatosUsuarios.Correo;
+                    obj.Telefono = DatosUsuarios.Telefono;
+
                     InvBD.SubmitChanges();
                     Afectados = 1;
                 }
--- Inventario/Scrips/Internos/JS_Usuarios.js
+++ Inventario/Scrips/Internos/JS_Usuarios.js
@@ -1,39 +1,86 @@
-﻿LlenarCMBPrin();
+﻿
+LlenarCMBPrin();
 
 var imagen64;
-ConsultaUsuarios();
-function ConsultaUsuarios() {
+CrearAcordeonUsuarios();
+
+
+//$("#TxtFechaN").datepicker(
+//    {
+//        dateFormat: "dd/mm/yy",
+//        changeMonth: true,
+//        changeYear: true
+//    }
+//);
+
+
+
+
+
+//Crea el acordeón e inserta (los registros de la base de datos)
+function CrearAcordeonUsuarios() {
     $.get("/Usuario/ConsultaUsuarios", function (Data) {
-        CrearTablaUsuarios(Data);
-    }
-    );
+        //Accordeon(DatosProveedor, document.getElementById("accordion"));
+        AcordeonUsuarios(Data, document.getElementById("accordion"));
+    });
+    imagen64 = getBase64Image(document.getElementById("PBFoto"));
 }
-function CrearTablaUsuarios(Data) {
-    var CodigoHtmlTablaUsuarios = "";
-    CodigoHtmlTablaUsuarios += "<table id='tablas' class='table table table-sm' >";
-    CodigoHtmlTablaUsuarios += " <thead class='thead-dark'><tr><th>CURP</th><th>Nombre</th><th>Apellido Paterno</th><th>Apellido Materno</th><th>Telefono</th><th>Acción</thead>";
-    CodigoHtmlTablaUsuarios += "<tbody>";
+function AcordeonUsuarios(Data, CtrlBonis) {
+    var CodigoHTMLUsuarios = "";
     for (var i = 0; i < Data.length; i++) {
-        CodigoHtmlTablaUsuarios += "<tr>";
-        CodigoHtmlTablaUsuarios += "<td>" + Data[i].CURP + "</td>";
-        CodigoHtmlTablaUsuarios += "<td>" + Data[i].Nombre + "</td>";
-        CodigoHtmlTablaUsuarios += "<td>" + Data[i].ApellidosP + "</td>";
-        CodigoHtmlTablaUsuarios += "<td>" + Data[i].ApellidosM + "</td>";
-        CodigoHtmlTablaUsuarios += "<td>" + Data[i].Telefono + "</td>";
-        CodigoHtmlTablaUsuarios += "<td>";
-        CodigoHtmlTablaUsuarios += "<button class='btn btn-primary' onclick='abrirModal(" + Data[i].IdUsuarios + ")' data-toggle='modal' data-target='#ModalUsuario'><i class='fas fa-edit'></i></button>";
-        CodigoHtmlTablaUsuarios += "<button class='btn btn-danger' onclick='EliminarUsuario(" + Data[i].IdUsuarios + ",this)'><i class='fas fa-eraser'></i></button>";
-        CodigoHtmlTablaUsuarios += "<button class='btn btn-primary' onclick='abrirModalBloqueado(" + Data[i].IdUsuarios + ")' data-toggle='modal' data-target='#ModalBloqueado'><i class='fas fa-window-restore list__img'></i></button>";
+        if (i < 1) {
+            CodigoHTMLUsuarios += "<div class='card m-b-0'>";
+        }
+        else {
+            CodigoHTMLUsuarios += "<div class='card m-b-0 border-top'>";
+        }
+        CodigoHTMLUsuarios += "<div class='card-header' id='heading" + Data[i].IdUsuarios + "'>";
+        CodigoHTMLUsuarios += "<h5 class='mb-0'>";
+        CodigoHTMLUsuarios += "<a  data-toggle='collapse' data-target='#collapse" + Data[i].IdUsuarios + "' aria-expanded='false' aria-controls='collapse" + Data[i].IdUsuarios + "' class='collapsed'>";
+        //CodigoHTMLUsuarios += "<i class='m-r-5 mdi mdi-store' aria-hidden='true'></i>";
+        CodigoHTMLUsuarios += "<i class='m-r-5 fas fa-clipboard-list' aria-hidden='true'></i>";
+        CodigoHTMLUsuarios += "<span >" + Data[i].CURP + "</span>";
+        CodigoHTMLUsuarios += "</a>";
+        CodigoHTMLUsuarios += "</h5>";
+        //En el data-parent se modifica para que se de un solo clic y se oculten los demás
+        CodigoHTMLUsuarios += "<div id='collapse" + Data[i].IdUsuarios + "' class='collapse' aria-labelledby='headingOne' data-parent='#collapse' style=''>";
+        CodigoHTMLUsuarios += "<div class='card-body'>";
+        CodigoHTMLUsuarios += "<div class='row'>";
+        CodigoHTMLUsuarios += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Nombre: </strong>" + Data[i].Nombre + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Apellidos Paterno: </strong>" + Data[i].ApellidosP + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Apellidos Materno: </strong>" + Data[i].ApellidosM + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Fecha De Nacimiento: </strong>" + Data[i].FechaDeNacimiento + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>RFC: </strong>" + Data[i].RFC + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Estado: </strong>" + Data[i].Estado + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Municipio: </strong>" + Data[i].Municipio + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Localidad: </strong>" + Data[i].Localidad + "</div>";
 
-        CodigoHtmlTablaUsuarios += "</td>";
-        CodigoHtmlTablaUsuarios += "</tr>";
+        CodigoHTMLUsuarios += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Area: </strong>" + Data[i].NArea + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Sub Area: </strong>" + Data[i].NSArea + "</div>";
+
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>NoSS: </strong>" + Data[i].NoSS + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Correo: </strong>" + Data[i].Correo + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-5 col-sm-6 col-xs-6'><strong>Telefono: </strong>" + Data[i].Telefono + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Perfil </strong>" + Data[i].LvlPerfil + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Usuario: </strong>" + Data[i].Usuario + "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>FechaIngreso: </strong>" + Data[i].FechaIngreso + "</div>";
+        CodigoHTMLUsuarios += "</div>";
+
+        //  CodigoHTMLUsuarios += "<div class='col-md-7 col-sm-6 col-xs-6'><strong>Dirección: </strong>" + DatosProveedor[i].Direccion + "</div>";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "<div class='col-md-12 col-sm-12 col-xs-12 align-self-end'>";
+        CodigoHTMLUsuarios += "<button class='btn btn-success' onclick='abrirModal(" + Data[i].IdUsuarios + ")' data-toggle='modal' data-target='#dialogo1'><i class='fas fa-edit'></i></button> ";
+        CodigoHTMLUsuarios += "<button class='btn btn-danger' onclick='EliminarUsuario(" + Data[i].IdUsuarios + ",this)' ><i class='fas fa-eraser'></i></button>";
+        CodigoHTMLUsuarios += "<button class='btn btn-success' onclick='abrirModalBloqueado(" + Data[i].IdUsuarios + ")' data-toggle='modal' data-target='#ModalBloqueado'><i class='fas fa-window-restore list__img'></i></button> ";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "</div>";
+        CodigoHTMLUsuarios += "</div>";
     }
-    CodigoHtmlTablaUsuarios += "</tbody>";
-    CodigoHtmlTablaUsuarios += "</table>";
-    document.getElementById("TablaUsuarios").innerHTML = CodigoHtmlTablaUsuarios;
+    CtrlBonis.innerHTML = CodigoHTMLUsuarios;
 }
-
-
 
 BloquearCTRL();
 
@@ -43,7 +90,6 @@ function BloquearCTRL() {
         $("#" + CTRL[i].id).attr('disabled', 'disabled');
     }
 }
-
 
 
 
@@ -59,7 +105,6 @@ function LlenarCMBPrin() {
         llenarCombo(data, document.getElementById("cmbPerfil"));
     });
 }
-
 
 //event Change index Areas para llenar el combobox SubAreas
 var IDA = document.getElementById("cmbArea");
@@ -197,7 +242,7 @@ IDM.addEventListener("change", function () {
 });
 
 
-//Guarda los cambios y altas de los proveedores 
+//Guarda los cambios y altas de los proveedores
 function GuardarUsuario() {
     var pas1 = document.getElementById("Txtpassword").value;
     var pas2 = document.getElementById("TxtConfirmacion").value;
@@ -284,11 +329,11 @@ function GuardarUsuario() {
                             alert("Ocurrio un error");
                         }
                         else if (data == -1) {
-                            alert("Ya existe el usuario");
+                            alert("Ya existe el proveedor");
                         }
                         else {
-                            alert("Se ejecutó correctamente");
-                            CrearTablaUsuarios();
+                            alert("Se ejecuto correctamente");
+                            CrearAcordeonUsuarios();
                             document.getElementById("btnCancelar").click();
                         }
                     }
@@ -349,8 +394,6 @@ function EliminarUsuario(id) {
 }
 
 
-
-
 //    //         //     //
 
 
@@ -408,3 +451,7 @@ function abrirModalBloqueado(id) {//la clase  Obligatorio
         });
     }
 }
+
+
+
+
