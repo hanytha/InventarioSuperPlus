5db8ddd49018f82513aa06afd18cc8358a70a598
AlmaLeticiaHernandezMartinez <alma.ytel@gmail.com>
2021-11-27 17:30:45 -0600

    Se comentó algunos apartados

--- Inventario/Models/ModeloPagina.cs
+++ Inventario/Models/ModeloPagina.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace Inventario.Models
 {
     public class ModeloPagina
-    {
+    {  //Modelo que se ocupa para crear el menú de opciones de manera dinámica mediante Razor
         public static List<long> IdPagina { get; set; }
         public static List<string> Mensaje { get; set; }
         public static List<long> Accion { get; set; }
