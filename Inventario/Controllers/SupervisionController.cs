a4d5b647b813610b0b3f033463e333f375b112f6
AlmaLeticiaHernandezMartinez <alma.ytel@gmail.com>
2021-12-02 09:07:57 -0600

    Se empezó a trabajar con el apartado SucursalesSupervisión

--- Inventario/Controllers/SupervisionController.cs
+++ Inventario/Controllers/SupervisionController.cs
@@ -115,6 +115,7 @@ namespace Inventario.Controllers
             return nregistradosAfectados;
         }
 
+        //--------------------Controlador SucursalesSupervision--------------------
 public ActionResult SucursalesSupervision()
         {
             return View();
@@ -248,6 +249,70 @@ public ActionResult SucursalesSupervision()
         }
 
 
-     
+        //---------------Tabla de las tiendas---------------
+
+        public JsonResult ConsultaArticulos()
+        {
+            string id = "";
+            string Nombre = "";
+            string Fechas = "";//Es la fecha de la ultima compra reaizada
+            string Stock = "";//Es la suma del stock atcual de todas las compras
+            string Costos = "";//Es el costo de la compra que actualmente se esta consumiendo
+
+            var ConsultaArticulo = InvBD.Articulos.Where(p => p.Estatus.Equals(1))
+            .Select(p => new
+            {
+                Id = p.IdArticulos,
+                nombres = p.NombreEmpresa
+            });
+            foreach (var art in ConsultaArticulo)
+            {
+                id += art.Id + ",";
+                Nombre += art.nombres + ",";
+                var consultaFecha = InvBD.Compra.Where(p => p.IdArticulo.Equals(art.Id) && p.ExitenciaActual > 0).OrderBy(p => p.IdCompra)
+                    .Select(p => new
+                    {
+                        fechaIngreso = p.FechaDeIngreso,
+                        stockActual = p.ExitenciaActual,
+                        costo = p.Coste,
+                    });
+
+                if (consultaFecha.Count() > 0)
+                {
+                    int UltimoReg = consultaFecha.Count() - 1;
+                    int cont = 0;
+                    int SumaStock = 0;
+                    //inicia
+                    //DateTime FultCompra;                
+                    foreach (var comp in consultaFecha)
+                    {
+
+                        SumaStock = (int)(SumaStock + comp.stockActual);
+                        if (cont == 0)
+                        {
+                            Costos += comp.costo + ",";
+                        }
+                        if (cont == UltimoReg)
+                        {
+                            Fechas += comp.fechaIngreso + ",";
+                        }
+                        cont++;
+                    }
+                    Stock += SumaStock + ",";
+                    //termina
+                }
+                else
+                {
+                    Costos += "0" + ",";
+
+                    Fechas += "2010-08-10" + ",";
+                    Stock += "0" + ",";
+                }
+            }
+            var Resultado = new { id = id.Substring(0, id.Length - 1), Nombre = Nombre.Substring(0, Nombre.Length - 1), Fechas = Fechas.Substring(0, Fechas.Length - 1), Stock = Stock.Substring(0, Stock.Length - 1), Costos = Costos.Substring(0, Costos.Length - 1) };
+            return Json(Resultado, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
--- Inventario/Scrips/Internos/JS_SupervisionTienda.js
+++ Inventario/Scrips/Internos/JS_SupervisionTienda.js
@@ -40,3 +40,129 @@ function llenarCombo(DAtos, control) {
     }
     control.innerHTML = contenido;
 }
+
+
+////----------Tabla------------////
+
+//-----------------------Crea el grid con las consultas de la tabla artículos y compra---------------------------------------------------
+//ConsultaArticuloComp();
+//function ConsultaArticuloComp() {
+//    $.get("/Prueba/ConsultaArticulos", function (Data) {
+//        CrearArticuloComp(Data);
+//    });
+
+//}
+
+//function CrearArticuloComp(Data) {
+//    var CodigoHtmlArticuloComp = "";
+//    CodigoHtmlArticuloComp += "<div id='contenedor1'>";
+//    CodigoHtmlArticuloComp += "<hr class='solid'>";
+//    CodigoHtmlArticuloComp += "<div class='row'>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>ID</div>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>Artículo</div>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>Fecha Ingreso</div>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>Stock</div>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>Costo</div>";
+//    CodigoHtmlArticuloComp += "<div class='col-sm'>Acción</div>";
+//    CodigoHtmlArticuloComp += "</div>";
+//    CodigoHtmlArticuloComp += "<hr class='solid'>";
+//    CodigoHtmlArticuloComp += "</div>";
+
+
+//    let id = Data.id;
+//    let ArrayId = id.split(',');
+//    let Nombre = Data.Nombre;
+//    let Arraynombre = Nombre.split(',');
+//    let Fechas = Data.Fechas;
+//    let Arrayfechas = Fechas.split(',');
+//    let Stock = Data.Stock;
+//    let Arraystock = Stock.split(',');
+//    let Costos = Data.Costos;
+//    let Arraycostos = Costos.split(',');
+
+
+//    for (var i = 0; i < (ArrayId, Arraynombre, Arrayfechas, Arraystock, Arraycostos).length; i++) {
+
+//        CodigoHtmlArticuloComp += "<div>";
+//        CodigoHtmlArticuloComp += "<div class='row'>";
+//        CodigoHtmlArticuloComp += "<div class='col-sm'>" + ArrayId[i] + "</div>";
+//        CodigoHtmlArticuloComp += "<div class='col-sm'>" + Arraynombre[i] + "</div>";
+//        CodigoHtmlArticuloComp += "<div class='col-sm'>" + Arrayfechas[i] + "</div>";
+//        CodigoHtmlArticuloComp += "<div class='col-sm'>" + Arraystock[i] + "</div>";
+//        CodigoHtmlArticuloComp += "<div class='col-sm'>" + Arraycostos[i] + "</div>";
+//        //-----------------Botón para desplegar la primera tabla--------------
+//        CodigoHtmlArticuloComp += "<div class='col'>"
+//        CodigoHtmlArticuloComp += "<label>"
+//        CodigoHtmlArticuloComp += "<button title='Clic para desplegar' class='btn btn-outline-primary' onclick='Desplegar(" + ArrayId[i] + ")' type='button' data-toggle='collapse' data-target='#desplegable" + ArrayId[i] + "' aria-expanded='false' aria-controls='desplegable(" + ArrayId[i] + ")'><i class='fas fa-angle-down'></i></button>";
+//        CodigoHtmlArticuloComp += "</label>";
+//        CodigoHtmlArticuloComp += "</div>";
+//        //-------------Termina----------------------------------------
+//        CodigoHtmlArticuloComp += "</div>";
+//        CodigoHtmlArticuloComp += "</div>";
+//        //------------------------Despliega primer grid-----------------------------------------------------------------------
+//        CodigoHtmlArticuloComp += "<div class='row'>";
+//        CodigoHtmlArticuloComp += "<div class='col'><div id='desplegable" + ArrayId[i] + "' class='collapse'></div></div>";
+//        CodigoHtmlArticuloComp += "</div>";
+//        //---------------------------------------Termina----------------------------------------------------------------------------
+//    }
+//    document.getElementById("contenedor1").innerHTML = CodigoHtmlArticuloComp;
+
+//}
+
+////----------------------------Crea el grid a desplegar con el botón con la funciíon de desplegar------------------------------------
+//function Desplegar(id) {
+
+//    if (id == 0) {
+//        sessionStorage.setItem('IDArt', '0');
+//    }
+//    else {
+
+//        $.get("/Prueba/ConsultaCompraJoinProveedor/?Id=" + id, function (Data) {
+//            var uno = "";
+//            //---Encabezado del grid---------
+//            uno += "<hr class='solid4'>";
+//            uno += "<div class='row'>";
+//            uno += "<div class='col-sm'>NoCompra</div>";
+//            uno += "<div class='col-sm'>Artículo</div>";
+//            uno += "<div class='col-sm'>Fecha de Ingreso</div>";
+//            uno += "<div class='col-sm'>Costo</div>";
+//            uno += "<div class='col-sm'>Proveedor</div>";
+//            uno += "<div class='col-sm'>Acción</div>";
+//            uno += "</div>";
+//            uno += "<hr class='solid4'>";
+
+
+
+//            for (var i = 0; i < Data.length; i++) {
+//                //----Cuerpo del grid-------------
+//                uno += "<div class='row'>";
+//                uno += "<div class='col-sm'>" + Data[i].NoCompra + "</div>";
+//                uno += "<div class='col-sm'>" + Data[i].Articulo + "</div>";
+//                uno += "<div class='col-sm'>" + Data[i].FechaDeIngreso + "</div>";
+//                uno += "<div class='col-sm'>" + Data[i].Coste + "</div>";
+//                //--------------------------------------------------------------------------------
+//                uno += "<div class='col-sm'><button style='background-color:white; border:none;' onclick='abrirModal(" + Data[i].IdProveedor + ")' data-toggle='modal' data-target='#ModalProveedor'>" + Data[i].Proveedor + "</button></div>";
+//                //-----------------Botón para desplegar la segunda tabla--------------------------------------------
+//                uno += "<div class='col-sm'>"
+//                uno += "<label>"
+//                uno += "<button title='Clic para desplegar Artículos de la misma compra' class='btn btn-outline-warning' onclick='Desplegar2(" + Data[i].NoCompra + ")' type='button' data-toggle='collapse' data-target='#desplegable2" + Data[i].NoCompra + "' aria-expanded='false' aria-controls='desplegable2(" + Data[i].NoCompra + ")'><i class='fas fa-angle-down'></i></button>";
+//                uno += "</label>"
+//                uno += "</div>";
+//                //-------------------Termina-------------------------
+//                uno += "</div>";
+//                //-----------------------------------------
+//                uno += "<div class='row'>";
+//                uno += "<div class='col'><div id='desplegable2" + Data[i].NoCompra + "' class='collapse'></div></div>";
+//                uno += "</div>";
+//            }
+//            uno += "</div>";
+//            uno += "</br>";
+//            uno += "</br>";
+
+//            let compraArticulo = "desplegable" + id;
+//            document.getElementById(compraArticulo).innerHTML = uno;
+//        });
+//    }
+//}
+**************************************************************************************************************
+**********************************************************************************************
