8adae37dd0f07d8b6f4c94eefa632558fed870f6
AnabelAquinoCaballero <leb2198@gmail.com>
2021-12-11 10:59:57 -0600

    Se agrego estilos de boostrap al input search de pedidos y el icono de buscar

--- Inventario/Scrips/Internos/JS_PedidosExt.js
+++ Inventario/Scrips/Internos/JS_PedidosExt.js
@@ -8,8 +8,11 @@ function ConsultaPedidos() {
 }
 function CrearTablaPedidos(Data) {
     var CodigoHtmlTablaPedidos = "";
-    CodigoHtmlTablaPedidos += "<input class='form-control col-md-3 light-table-filter' data-table='order-table' type='text' placeholder='Search..'>"
-    CodigoHtmlTablaPedidos +="<div class='table-responsive'>"
+    CodigoHtmlTablaPedidos += "<div class='input-group mb-3'>";
+    CodigoHtmlTablaPedidos += "<span  class='input-group-text' id='basic-addon1'><i class='fas fa-search'></i></span>";
+    CodigoHtmlTablaPedidos += "<input  class='form-control col-md-3 light-table-filter' data-table='order-table' type='text' placeholder='Search..'>";
+    CodigoHtmlTablaPedidos += "</div>";
+    CodigoHtmlTablaPedidos += "<div class='table-responsive'>";
     CodigoHtmlTablaPedidos += "<table class='table-primary table table-bordered order-table'>";
     CodigoHtmlTablaPedidos += "<thead>";
     CodigoHtmlTablaPedidos += "<tr>";
