d6160674afbc190ac988e78598da4950335cbb62
AnabelAquinoCaballero <leb2198@gmail.com>
2022-02-22 11:43:14 -0600

    Se agrego el modelo de mermas

---
+++ Inventario/Models/ModeloMermas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Models
+{
+    public class ModeloMermas
+    {
+        public static List<long> IdMermas { get; set; }
+        public static List<long> IdCompra { get; set; }
+        public static List<long> IdArticulo { get; set; }
+        public static List<long> StockInicial { get; set; }
+        public static List<long> StockActual { get; set; }
+        public static List<string> Articulo { get; set; }
+        public static List<string> Observaciones { get; set; }
+
+    }
+}
