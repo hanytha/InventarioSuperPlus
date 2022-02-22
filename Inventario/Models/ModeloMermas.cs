216bc0959f8119881085f790e9f0759a8f103fba
AnabelAquinoCaballero <leb2198@gmail.com>
2022-02-22 15:04:03 -0600

    Corrección del modelo

--- Inventario/Models/ModeloMermas.cs
+++ Inventario/Models/ModeloMermas.cs
@@ -10,6 +10,18 @@ namespace Inventario.Models
         public static List<long> IdCompraInterno { get; set; }
         public static List<long> IdProveedor { get; set; }
         public static List<string> Proveedor { get; set; }
-    
+
+
+        public static List<long> IdExistenciaAlmacenG { get; set; }
+        public static List<long> IdCompra { get; set; }
+        public static List<long> IdArticulo { get; set; }
+        public static List<long> ExitenciaInicial { get; set; }
+        public static List<long> ExitenciaActual { get; set; }
+        public static List<long> NoPedidoG { get; set; }
+        public static List<string> TipoDeOperacion { get; set; }
+        public static List<string> Observaciones { get; set; }
+
+
+
     }
 }
