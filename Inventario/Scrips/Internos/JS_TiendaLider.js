c198f707e2d1ce3c0ba85e003289aa905318c478
AlmaLeticiaHernandezMartinez <alma.ytel@gmail.com>
2022-02-26 11:51:59 -0600

    Subiendo cambios

--- Inventario/Scrips/Internos/JS_TiendaLider.js
+++ Inventario/Scrips/Internos/JS_TiendaLider.js
@@ -221,8 +221,8 @@ function ConsultaPedidosRealizados(IDTienda) {
                 CodigoHtmlArticuloComp += "<div class='col'>"
                 CodigoHtmlArticuloComp += "<label>"
                 //Pasar los 2 parámetros de la función desplegar(función que muestra la tabla del artículo) para  conocer el número de pedido que se va a mostrar en la tienda que tenga el id recibido
-             //   CodigoHtmlArticuloComp += "<button title='Clic para desplegar' class='btn btn-outline-primary' onclick='Desplegar(" + ArrayIdCmpraInt[i] + "," + ArrayIdSitio[i] + ")' type='button' data-toggle='collapse' data-target='#desplegable" + ArrayIdCmpraInt[i] + "," + ArrayIdSitio[i] + "' aria-expanded='false' aria-controls='desplegable(" + ArrayIdCmpraInt[i] + ", " + ArrayIdSitio[i] + ")'><i class='fas fa-angle-down'></i></button>";
-              //  CodigoHtmlArticuloComp += "<button title='Pedido' class='btn btn-primary' onclick='VerPedido(" + ArrayId[i] + "," + ArrayNoPedido[i] + "," + ArrayIdCmpraInt[i] + "," + ArrayIdSitio[i] + ")'data-toggle='modal' data-target='#ModalPedidos'><i class='fas fa-archive'></i></button>";
+                CodigoHtmlArticuloComp += "<button title='Clic para desplegar' class='btn btn-outline-primary' onclick='Desplegar(" + NoPedido[i] + "," + ArrayIdSitio[i] + ")' type='button' data-toggle='collapse' data-target='#desplegable" + NoPedido[i] + "," + ArrayIdSitio[i] + "' aria-expanded='false' aria-controls='desplegable(" + NoPedido[i] + ", " + ArrayIdSitio[i] + ")'><i class='fas fa-angle-down'></i></button>";
+                CodigoHtmlArticuloComp += "<button title='Pedido' class='btn btn-primary' onclick='VerPedido(" + ArrayId[i] + "," + ArrayNoPedido[i] + "," + ArrayNoPedido[i] + "," + ArrayIdSitio[i] + ")'data-toggle='modal' data-target='#ModalPedidos'><i class='fas fa-archive'></i></button>";
 
                 CodigoHtmlArticuloComp += "</div>";
                 //-------------Termina----------------------------------------
@@ -230,7 +230,7 @@ function ConsultaPedidosRealizados(IDTienda) {
                 CodigoHtmlArticuloComp += "</div>";
                 //------------------------Despliega primer grid-----------------------------------------------------------------------
                 CodigoHtmlArticuloComp += "<div class='row'>";
-               // CodigoHtmlArticuloComp += "<div class='col'><div id='desplegable" + ArrayIdCmpraInt[i] + "," + ArrayIdSitio[i] + "' class='collapse'></div></div>";
+                CodigoHtmlArticuloComp += "<div class='col'><div id='desplegable" + NoPedido[i] + "," + ArrayIdSitio[i] + "' class='collapse'></div></div>";
                 CodigoHtmlArticuloComp += "</div>";
                 //---------------------------------------Termina----------------------------------------------------------------------------
             }
@@ -429,6 +429,8 @@ function Desplegar(IdCmpraInt, id) {
         });
     }
 }
+
+
 
 function abrirModalDevoluciones(idExist, id, idS) {
     ObtenerFecha();
@@ -1806,4 +1808,68 @@ function nuevoStock() {
 //        if (Data == 1) { alert("--Guardado---") }
 
 //    });
+//}
+
+
+///////////////////////////Aceptar pedidos--------------------------
+
+//function Desplegar(IdCmpraInt, id) {
+//    if (IdCmpraInt == 0 && id == 0) {
+//        sessionStorage.setItem('IDArt', '0');
+//    }
+//    else {
+//        $.get("/Supervision/ConsultaArtTiendaLider/?idCompraInt=" + IdCmpraInt + "&idS= " + id, function (Data) {
+//            var DespXArt = "";
+//            //---Encabezado del grid---------
+//            DespXArt += "<hr class='solid4'>";
+//            DespXArt += "<div class='row'>";
+//            DespXArt += "<div class='col-sm'>NoPedido</div>";
+//            DespXArt += "<div class='col-sm'>Artículo</div>";
+//            DespXArt += "<div class='col-sm'>Fecha de Ingreso</div>";
+//            DespXArt += "<div class='col-sm'>Stock</div>";
+//            DespXArt += "<div class='col-sm'></div>";
+
+//            DespXArt += "</div>";
+//            DespXArt += "<hr class='solid4'>";
+
+
+//            let IdArticulo = Data.IdArticulo;
+//            let ArrayIdArticulos = IdArticulo.split(',');
+//            let NumeroPedido = Data.NumeroPedido;
+//            let ArrayNoPedido = NumeroPedido.split(',');
+//            let Fechas = Data.Fechas;
+//            let Arrayfecha = Fechas.split(',');
+//            let Stock = Data.Stock;
+//            let Arraystock = Stock.split(',');
+//            //El IdSitio se ocupa para conocer en qué tienda mostrar los pedidos
+//            let IdSitio = Data.IdSitio;
+//            let ArrayIdSitio = IdSitio.split(',');
+
+//            let Nombre = Data.Nombre;
+//            let ArrayArticulo = Nombre.split(',');
+//            let IdExistenciaAlmacenG = Data.IdExistenciaAlmacenG;
+//            let ArrayIdExistenciaAlmacenG = IdExistenciaAlmacenG.split(',');
+
+//            for (var i = 0; i < ArrayIdArticulos.length; i++) {
+//                //----Cuerpo del grid-------------
+//                DespXArt += "<div class='row'>";
+
+//                DespXArt += "<div class='col-sm'>" + ArrayNoPedido[i] + "</div>";
+//                DespXArt += "<div class='col-sm'>" + ArrayArticulo[i] + "</div>";
+//                DespXArt += "<div class='col-sm'>" + Arrayfecha[i] + "</div>";
+//                DespXArt += "<div class='col-sm'>" + Arraystock[i] + "</div>";
+//                DespXArt += "<button title='Devoluciones' class='btn btn-primary' onclick='abrirModalDevoluciones(" + ArrayIdExistenciaAlmacenG[i] + "," + ArrayIdArticulos[i] + "," + ArrayIdSitio[i] + ")'data-toggle='modal' data-target='#ModalDevoluciones'><i class='fas fa-archive'></i></button>";
+//                DespXArt += "<div class='col-sm'></div>";
+//                DespXArt += "</div>";
+
+//            }
+//            DespXArt += "</div>";
+//            DespXArt += "</br>";
+//            DespXArt += "</br>";
+//            //Pasando los dos parametros(No.Pedido, idSitio) para desplegar los articulos del pedido por tienda
+//            let compraArticulo = "desplegable" + IdCmpraInt + "," + id;
+//            document.getElementById(compraArticulo).innerHTML = DespXArt;
+
+//        });
+//    }
 //}
