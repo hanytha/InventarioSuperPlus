52118b08cb897f4c95421cd4c2a43cd089e56d81
AnabelAquinoCaballero <leb2198@gmail.com>
2022-02-28 14:03:55 -0600

    Corecciones

---
+++ Inventario/Controllers/ComprasIntAreasController.cs
@@ -0,0 +1,24 @@
+﻿using Inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    //Lamar al método de seguridad
+    [Seguridad]
+
+    public class ComprasIntAreasController : Controller
+    {
+        //conexion con DB
+        InventarioBDDataContext InvBD = new InventarioBDDataContext();
+
+        // GET: ComprasIntAreas
+        public ActionResult ComprasIntAreas()
+        {
+            return View();
+        }
+    }
+}
