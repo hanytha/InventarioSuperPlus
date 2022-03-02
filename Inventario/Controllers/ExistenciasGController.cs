2f23e7c39e89422bf1d4fd4d58e53f53c4e75f03
AnabelAquinoCaballero <leb2198@gmail.com>
2022-03-02 11:48:32 -0600

    Cambios

--- Inventario/Controllers/ExistenciasGController.cs
+++ Inventario/Controllers/ExistenciasGController.cs
@@ -16,75 +16,11 @@ namespace Inventario.Controllers
         // GET: Prueba
         public ActionResult ExistenciasG()
         {
-            ExistenciasGController departamento = new ExistenciasGController();
-            departamento.BDDepartamento();
             ExistenciasGController vista = new ExistenciasGController();
             vista.ConsultaArticulosArea();
             return View();
         }
-        //public JsonResult ConsultaArticulos()
-        //{
-        //    string id = "";
-        //    string Nombre = "";
-        //    string Fechas = "";//Es la fecha de la ultima compra reaizada
-        //    string Stock = "";//Es la suma del stock atcual de todas las compras
-        //    string Costos = "";//Es el costo de la compra que actualmente se esta consumiendo
-        //    var ConsultaArticulo = InvBD.Articulos.Where(p => p.Estatus.Equals(1))
-        //    .Select(p => new
-        //    {
-        //        Id = p.IdArticulos,
-        //        nombres = p.NombreEmpresa
-        //    });
-        //    foreach (var art in ConsultaArticulo)
-        //    {
-        //        id += art.Id + ",";
-        //        Nombre += art.nombres + ",";
-        //        var consultaFecha = InvBD.ComprasArticulos.Where(p => p.IdArticulo.Equals(art.Id) && p.StockActual > 0).OrderBy(p => p.IdCompra)
-        //            .Select(p => new
-        //            {
-        //                fechaIngreso = p.FechaIngreso,
-        //                stockActual = p.StockActual,
-        //                costo = p.PrecioUnitario,
-        //            });
 
-        //        if (consultaFecha.Count() > 0)
-        //        {
-        //            int UltimoReg = consultaFecha.Count() - 1;
-        //            int cont = 0;
-        //            int SumaStock = 0;
-        //            //inicia
-        //            //DateTime FultCompra;                
-        //            foreach (var comp in consultaFecha)
-        //            {
-
-        //                SumaStock = (int)(SumaStock + comp.stockActual);
-
-        //                if (cont == UltimoReg)
-        //                {
-        //                    Fechas += comp.fechaIngreso + ",";
-        //                    Costos += comp.costo + ",";
-        //                }
-        //                cont++;
-        //            }
-        //            Stock += SumaStock + ",";
-        //            //termina
-        //        }
-        //        else
-        //        {
-        //            Costos += "0" + ",";
-
-        //            Fechas += "2010-08-10" + ",";
-        //            Stock += "0" + ",";
-        //        }
-        //    }
-        //    var Resultado = new { id = id.Substring(0, id.Length - 1),
-        //        Nombre = Nombre.Substring(0, Nombre.Length - 1),
-        //        Fechas = Fechas.Substring(0, Fechas.Length - 1), 
-        //        Stock = Stock.Substring(0, Stock.Length - 1), 
-        //        Costos = Costos.Substring(0, Costos.Length - 1) };
-        //    return Json(Resultado, JsonRequestBehavior.AllowGet);
-        //}
-        //-------------------------------------------------------------------------------------------------------------
         //*****************************************************************************************************************
         public void ConsultaArticulosArea()
         {
