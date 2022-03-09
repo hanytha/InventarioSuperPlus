4c047ab312089e6bdc46d880ecca7c2eb13b46ce
AnabelAquinoCaballero <leb2198@gmail.com>
2022-03-09 09:32:54 -0600

    Generando el menú con variables de sesión

--- Inventario/Controllers/LoginController.cs
+++ Inventario/Controllers/LoginController.cs
@@ -64,113 +64,179 @@ namespace Inventario.Controllers
                             p.Password
                         }).First();
 
-                        Session["Usuario"] = DatosUsuario.IdUsuarios;
-                        Accesos.Id = DatosUsuario.IdUsuarios;
-                        Accesos.CURP = DatosUsuario.CURP;
-                        Accesos.Nombre = DatosUsuario.Nombre;
-                        Accesos.ApellidosP = DatosUsuario.ApellidosP;
-                        Accesos.ApellidosM = DatosUsuario.ApellidosM;
-                        Accesos.CURP = DatosUsuario.CURP;
-                        Accesos.Usuario = DatosUsuario.Usuario;
-                        Accesos.Foto = "data:image/png;base64," + Convert.ToBase64String(DatosUsuario.Foto.ToArray());
-                        Accesos.FechaDeNacimiento = DatosUsuario.FechaDeNacimiento;
-                        Accesos.IdEstado = DatosUsuario.IdEstado;
-                        Accesos.IdMunicipio = DatosUsuario.IdMunicipio;
-                        Accesos.IdLocalidad = DatosUsuario.IdLocalidad;
-                        Accesos.RFC = DatosUsuario.RFC;
-                        Accesos.NoSS = DatosUsuario.NoSS;
-                        Accesos.Correo = DatosUsuario.Correo;
-                        Accesos.Telefono = DatosUsuario.Telefono;
+                        Session["IdUsuarios"] = (long)DatosUsuario.IdUsuarios;
+                        Session["Usuario"] = DatosUsuario.Usuario;
+                        Session["CURP"] = DatosUsuario.CURP;
+                        Session["Nombre"] = DatosUsuario.Nombre;
+                        Session["ApellidosP"] = DatosUsuario.ApellidosP;
+                        Session["ApellidosM"] = DatosUsuario.ApellidosM;
+                        Session["FechaDeNacimiento"] = DatosUsuario.FechaDeNacimiento;
+                        Session["IdEstado"] = (long)DatosUsuario.IdEstado;
+                        Session["IdMunicipio"] = (long)DatosUsuario.IdMunicipio;
+                        Session["IdLocalidad"] = (long)DatosUsuario.IdLocalidad;
+                        Session["RFC"] = DatosUsuario.RFC;
+                        Session["NoSS"] = DatosUsuario.NoSS;
+                        Session["Correo"] = DatosUsuario.Correo;
+                        Session["Telefono"] = DatosUsuario.Telefono;
                         Session["IdPerfil"] = (long)DatosUsuario.IdPerfil;
-                        Accesos.IdPerfil = DatosUsuario.IdPerfil;
-                        Accesos.LvlPerfil = DatosUsuario.LvlPerfil;
-                        Accesos.FechaIngreso = DatosUsuario.FechaIngreso;
-                        Accesos.Estado = DatosUsuario.Estado;
-                        Accesos.Municipio = DatosUsuario.Municipio;
-                        Accesos.Localidad = DatosUsuario.Localidad;
-                        Accesos.IdArea = DatosUsuario.IdArea;
-                        Accesos.IdSubArea = DatosUsuario.IdSubArea;
-                        Accesos.NArea = DatosUsuario.NArea;
-                        Accesos.NSArea = DatosUsuario.NSArea;
+                        Session["LvlPerfil"] = DatosUsuario.LvlPerfil;
+                        Session["FechaIngreso"] = DatosUsuario.FechaIngreso;
+                        Session["Estado"] = DatosUsuario.Estado;
+                        Session["Municipio"] = DatosUsuario.Municipio;
+                        Session["Localidad"] = DatosUsuario.Localidad;
+                        Session["IdArea"] = (long)DatosUsuario.IdArea;
+                        Session["IdSubArea"] = (long)DatosUsuario.IdSubArea;
+                        Session["NArea"] = DatosUsuario.NArea;
+
+                        Session["NSArea"] = DatosUsuario.NSArea;
+                        Session["NombreSitio"] = DatosUsuario.NombreSitio;
                         Session["IDAsignacion"] = (long)DatosUsuario.IdAsignacion;
-                        Accesos.IDAsignacion = (long)DatosUsuario.IdAsignacion;
                         Session["IDSitio"] = (long)DatosUsuario.IdSitio;
-                        Accesos.IDSitio = (long)DatosUsuario.IdSitio;
+
+                        //Accesos.Id = DatosUsuario.IdUsuarios;
+                        //Accesos.CURP = DatosUsuario.CURP;
+                        //Accesos.Nombre = DatosUsuario.Nombre;
+                        //Accesos.ApellidosP = DatosUsuario.ApellidosP;
+                        //Accesos.ApellidosM = DatosUsuario.ApellidosM;;
+                        //Accesos.Usuario = DatosUsuario.Usuario;
+                        //Accesos.Foto = "data:image/png;base64," + Convert.ToBase64String(DatosUsuario.Foto.ToArray());
+                        //Accesos.FechaDeNacimiento = DatosUsuario.FechaDeNacimiento;
+                        //Accesos.IdEstado = DatosUsuario.IdEstado;
+                        //Accesos.IdMunicipio = DatosUsuario.IdMunicipio;
+                        //Accesos.IdLocalidad = DatosUsuario.IdLocalidad;
+                        //Accesos.RFC = DatosUsuario.RFC;
+                        //Accesos.NoSS = DatosUsuario.NoSS;
+                        //Accesos.Correo = DatosUsuario.Correo;
+                        //Accesos.Telefono = DatosUsuario.Telefono;
+                        //Session["IdPerfil"] = (long)DatosUsuario.IdPerfil;
+                        //Accesos.IdPerfil = DatosUsuario.IdPerfil;
+                        //Accesos.LvlPerfil = DatosUsuario.LvlPerfil;
+                        //Accesos.FechaIngreso = DatosUsuario.FechaIngreso;
+                        //Accesos.Estado = DatosUsuario.Estado;
+                        //Accesos.Municipio = DatosUsuario.Municipio;
+                        //Accesos.Localidad = DatosUsuario.Localidad;
+                        //Accesos.IdArea = DatosUsuario.IdArea;
+                        //Accesos.IdSubArea = DatosUsuario.IdSubArea;
+                        //Accesos.NArea = DatosUsuario.NArea;
+                        //Accesos.NSArea = DatosUsuario.NSArea;
+                        //Session["IDAsignacion"] = (long)DatosUsuario.IdAsignacion;
+                        //Accesos.IDAsignacion = (long)DatosUsuario.IdAsignacion;
+                        //Session["IDSitio"] = (long)DatosUsuario.IdSitio;
+                        //Accesos.IDSitio = (long)DatosUsuario.IdSitio;
                         //Accesos.Sitio = DatosUsuario.IDSitio;
-                        
 
 
 
-                        if ((long)Session["IDAsignacion"] != 0)
+
+                        if (Accesos.IDAsignacion != 0)
                         {
-                            var Asignasion = InvBD.Asignacion.Where(p => p.IdAsignacion.Equals(DatosUsuario.IdAsignacion)).First();
-                            Accesos.Asignacion = Asignasion.Nombre;
-                            if (Accesos.IDAsignacion == 1)
+                            var Asignasion = InvBD.Asignacion.Where(p => p.IdAsignacion.Equals(Session["IDAsignacion"])).First();
+                            Session["IDAsignacion"] = Asignasion.Nombre;
+                            if ((long)Session["IDAsignacion"] == 1)
                             {
-                                var Sucursal = InvBD.Tienda.Where(p => p.IdTienda.Equals(DatosUsuario.IdSitio)).First();
-                                Accesos.Sitio = Sucursal.Nombre;
-                                Accesos.Tiendas = DatosUsuario.IdSitio.ToString();
+                                var Sucursal = InvBD.Tienda.Where(p => p.IdTienda.Equals(Session["IDSitio"])).First();
+                                Session["IDSitio"] = Sucursal.Nombre;
+                                Session["Tiendas"] = DatosUsuario.IdSitio.ToString();
+
                             }
                             else if ((long)Session["IDAsignacion"] == 2)
                             {
-                                var Supervision = InvBD.Supervision.Where(p => p.IdSupervision.Equals(DatosUsuario.IdSitio)).First();
-                                Accesos.Tiendas = Supervision.Tienda;
-                                Accesos.Sitio = Supervision.TipoSupervicion;
+                                var Supervision = InvBD.Supervision.Where(p => p.IdSupervision.Equals(Session["IDSitio"])).First();
+                                Session["Tiendas"] = Supervision.Tienda;
+                                Session["IDSitio"] = Supervision.TipoSupervicion;
                             }
                             else if ((long)Session["IDAsignacion"] == 3)
                             {
-                                Accesos.Sitio = "Oficina";
-                                Accesos.Tiendas = "";
+                                Session["NombreSitio"] = "Oficina";
+                                Session["Tiendas"] = "";
                             }
                             else
                             {
-                                Accesos.Sitio = "No tiene ninguna asignación";
-                                Accesos.Tiendas = "";
+                                Session["NombreSitio"] = "No tiene ninguna asignación";
+                                Session["Tiendas"] = "";
                             }
                         }
-                        if (DatosUsuario.NSArea != "--Seleccione--")
+                        if ((string)Session["NSArea"] != "--Seleccione--")
                         {
-                            Accesos.NSArea = DatosUsuario.NSArea;
+                            Session["NSArea"] = Session["NSArea"];
                         }
                         else
                         {
-                            Accesos.NSArea = "";
+                            Session["NSArea"] = "";
                         }
 
 
-                       
+
                         var Permisos = InvBD.PerfilDeUsuario.Where(p => p.IdPerfilDeUsuario.Equals(Session["IdPerfil"])).First();
                         string[] IdPaginas = Permisos.Permisos.Split('#');
                         int Filas = IdPaginas.GetLength(0);
                         string[,] IdPagina = new string[Filas, 3];
-                        Accesos.Perfil = Permisos.Perfil;
-                        Accesos.NIvel = Permisos.Nivel;
-                        Accesos.Accion = new List<string>();
-                        Accesos.Controlador = new List<string>();
-                        Accesos.Mensaje = new List<string>();
-                        Accesos.Icono = new List<string>();
-                        Accesos.Estatus = new List<long>();
+                        Session["Perfil"] = Permisos.Perfil;
+                        Session["Nivel"] = Permisos.Nivel;
+
+
+
+                        string Accion = "";
+                        string Controlador = "";
+                        string Icono = "";
+                        string Mensaje = "";
+                        //string Estatus = "";
 
                         for (int i = 0; i < Filas; i++)
                         {
-                            var Pagina = InvBD.Pagina.Where(p => p.IdPagina.Equals(IdPaginas[i]))
+                            var Pagina = InvBD.Pagina.Where(p => p.IdPagina.Equals(IdPaginas[i]) && p.Estatus.Equals(1))
                                 .Select(p => new
                                 {
                                     p.Accion,
                                     p.Controlador,
                                     p.Mensaje,
-                                    p.Icono,
-                                    p.Estatus
+                                    p.Icono
+                                    //p.Estatus
                                 });
                             foreach (var item in Pagina)
                             {
-                                Accesos.Accion.Add(item.Accion);
-                                Accesos.Controlador.Add(item.Controlador);
-                                Accesos.Mensaje.Add(item.Mensaje);
-                                Accesos.Icono.Add(item.Icono);
-                                Accesos.Estatus.Add(item.Estatus);
+                                Accion += item.Accion + ",";
+                                Controlador += item.Controlador + ",";
+                                Mensaje += item.Mensaje + ",";
+                                Icono += item.Icono + ",";
+                                //Estatus += item.Estatus + ",";
                             }
+
                         }
+
+                        Accion = Accion.Substring(0, Accion.Length - 1);
+                        Controlador = Controlador.Substring(0, Controlador.Length - 1);
+                        Mensaje = Mensaje.Substring(0, Mensaje.Length - 1);
+                        Icono = Icono.Substring(0, Icono.Length - 1);
+                        //Estatus = Estatus.Substring(0, Estatus.Length - 1);
+                        Session["Acciones"] = Accion;
+                        Session["Controladores"] = Controlador;
+                        Session["Mensajes"] = Mensaje;
+                        Session["Iconos"] = Icono;
+
+
+                        //string[] Acciones = Accion.Substring(0, Accion.Length - 1).Split(',');
+                        //string[] Controladores = Controlador.Substring(0, Controlador.Length - 1).Split(',');
+                        //string[] Mensajes = Mensaje.Substring(0, Mensaje.Length - 1).Split(',');
+                        //string[] Iconos = Icono.Substring(0, Icono.Length - 1).Split(',');
+                        //string[] Estu = Estatus.Substring(0, Estatus.Length - 1).Split(',');
+
+                        //for (int i = 0; i < Acciones.GetLength(0); i++)
+                        //{
+                        //    Session["Acciones"] = Convert.ToString(Acciones[i]);
+                        //    Session["Controladores"] = Convert.ToString(Controladores[i]);
+                        //    Session["Mensajes"] = Convert.ToString(Mensajes[i]);
+                        //    Session["Iconos"] = Convert.ToString(Iconos[i]);
+                        //    Session["Estu"] = Convert.ToInt32(Estu[i]);
+                        //}
+
+                        //Session["Acciones"] = Acciones;
+                        //Session["Controladores"] = Controladores;
+                        //Session["Mensajes"] = Mensajes;
+                        //Session["Iconos"] = Iconos;
+                        //Session["Estu"] = Estu;
+
+
                     }
                 }
             }
